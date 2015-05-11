a4e1852e8593cb8cab8fa2065a7e94ea7a1fff2c
Laurent Declercq <l.declercq@nuxwin.com>
2015-05-11 08:03:27 +0200

    New upstream release 1.9.0.325 ( Community version )

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# i-MSCP RainLoop Package
+# i-MSCP RainLoop Community Package
 
-Repository which provides the i-MSCP RainLoop package ( composer ). This package is automatically fetched and installed
-by the i-MSCP installer when needed.
+Repository which provides the i-MSCP RainLoop Community package ( composer ). This package is automatically fetched and
+installed by the i-MSCP installer when selected.
 
-Upstream release: 1.8.0.251
+Upstream release: 1.9.0.325 ( Community version )
---
+++ src/rainloop/v/1.9.0.325/app/libraries/RainLoop/Enumerations/Capa.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace RainLoop\Enumerations;
+
+class Capa
+{
+	const PREM = 'PREM';
+	const TWO_FACTOR = 'TWO_FACTOR';
+	const TWO_FACTOR_FORCE = 'TWO_FACTOR_FORCE';
+	const OPEN_PGP = 'OPEN_PGP';
+	const PREFETCH = 'PREFETCH';
+	const GRAVATAR = 'GRAVATAR';
+	const THEMES = 'THEMES';
+	const USER_BACKGROUND = 'USER_BACKGROUND';
+	const SIEVE = 'SIEVE';
+	const FILTERS = 'FILTERS';
+	const ATTACHMENT_THUMBNAILS = 'ATTACHMENT_THUMBNAILS';
+	const ADDITIONAL_ACCOUNTS = 'ADDITIONAL_ACCOUNTS';
+	const TEMPLATES = 'TEMPLATES';
+	const AUTOLOGOUT = 'AUTOLOGOUT';
+}
