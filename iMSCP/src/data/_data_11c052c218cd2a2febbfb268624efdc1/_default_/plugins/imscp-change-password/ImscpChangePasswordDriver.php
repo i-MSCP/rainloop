b87fe88efe2f32a2bb687320fb8530e09003f1b1
Laurent Declercq <l.declercq@nuxwin.com>
2017-04-30 12:25:20 +0200

    Fixed: Password changer plugin for i-MSCP not compatible with Serie 1.4.x CS fixes

--- iMSCP/src/data/_data_11c052c218cd2a2febbfb268624efdc1/_default_/plugins/imscp-change-password/ImscpChangePasswordDriver.php
+++ iMSCP/src/data/_data_11c052c218cd2a2febbfb268624efdc1/_default_/plugins/imscp-change-password/ImscpChangePasswordDriver.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (C) 2015 Laurent Declercq <l.declercq@nuxwin.com>
+ * Copyright (C) 2015-2017 Laurent Declercq <l.declercq@nuxwin.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -22,131 +22,222 @@
  */
 class ImscpChangePasswordDriver implements \RainLoop\Providers\ChangePassword\ChangePasswordInterface
 {
-	/**
-	 * @var string
-	 */
-	private $sDsn = '';
+    const BASE64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
 
-	/**
-	 * @var string
-	 */
-	private $sUser = '';
+    /**
+     * @var string
+     */
+    private $sDsn = '';
 
-	/**
-	 * @var string
-	 */
-	private $sPassword = '';
+    /**
+     * @var string
+     */
+    private $sUser = '';
 
-	/**
-	 * @var string
-	 */
-	private $sAllowedEmails = '';
+    /**
+     * @var string
+     */
+    private $sPassword = '';
 
-	/**
-	 * @var \MailSo\Log\Logger
-	 */
-	private $oLogger = null;
+    /**
+     * @var string
+     */
+    private $sAllowedEmails = '';
 
-	/**
-	 * Set config
-	 *
-	 * @param string $sDsn
-	 * @param string $sUser
-	 * @param string $sPassword
-	 * @return \ImscpChangePasswordDriver
-	 */
-	public function SetConfig($sDsn, $sUser, $sPassword)
-	{
-		$this->sDsn = $sDsn;
-		$this->sUser = $sUser;
-		$this->sPassword = $sPassword;
+    /**
+     * @var \MailSo\Log\Logger
+     */
+    private $oLogger = NULL;
 
-		return $this;
-	}
+    /**
+     * Set config
+     *
+     * @param string $sDsn
+     * @param string $sUser
+     * @param string $sPassword
+     * @return \ImscpChangePasswordDriver
+     */
+    public function SetConfig($sDsn, $sUser, $sPassword)
+    {
+        $this->sDsn = $sDsn;
+        $this->sUser = $sUser;
+        $this->sPassword = $sPassword;
+        return $this;
+    }
 
-	/**
-	 * Set allowed emails
-	 *
-	 * @param string $sAllowedEmails
-	 * @return \ImscpChangePasswordDriver
-	 */
-	public function SetAllowedEmails($sAllowedEmails)
-	{
-		$this->sAllowedEmails = $sAllowedEmails;
+    /**
+     * Set allowed emails
+     *
+     * @param string $sAllowedEmails
+     * @return \ImscpChangePasswordDriver
+     */
+    public function SetAllowedEmails($sAllowedEmails)
+    {
+        $this->sAllowedEmails = $sAllowedEmails;
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Set logger
+     *
+     * @param \MailSo\Log\Logger $oLogger
+     * @return \ImscpChangePasswordDriver
+     */
+    public function SetLogger($oLogger)
+    {
+        if ($oLogger instanceof \MailSo\Log\Logger) {
+            $this->oLogger = $oLogger;
+        }
 
-	/**
-	 * Set logger
-	 *
-	 * @param \MailSo\Log\Logger $oLogger
-	 * @return \ImscpChangePasswordDriver
-	 */
-	public function SetLogger($oLogger)
-	{
-		if($oLogger instanceof \MailSo\Log\Logger) {
-			$this->oLogger = $oLogger;
-		}
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Check for password change possibility
+     *
+     * @param \RainLoop\Account $oAccount
+     * @return bool
+     */
+    public function PasswordChangePossibility($oAccount)
+    {
+        return $oAccount
+            && $oAccount->Email()
+            && \RainLoop\Plugins\Helper::ValidateWildcardValues($oAccount->Email(), $this->sAllowedEmails);
+    }
 
-	/**
-	 * Check for password change possibility
-	 *
-	 * @param \RainLoop\Account $oAccount
-	 * @return bool
-	 */
-	public function PasswordChangePossibility($oAccount)
-	{
-		return $oAccount && $oAccount->Email() && \RainLoop\Plugins\Helper::ValidateWildcardValues(
-			$oAccount->Email(), $this->sAllowedEmails
-		);
-	}
+    /**
+     * Change password
+     *
+     * @param \RainLoop\Account $oAccount
+     * @param string $sPrevPassword Previous password
+     * @param string $sNewPassword New password
+     * @return bool TRUE on success, FALSE on failure
+     */
+    public function ChangePassword(\RainLoop\Account $oAccount, $sPrevPassword, $sNewPassword)
+    {
+        if ($this->oLogger) {
+            $this->oLogger->Write('iMSCP: Try to change password for ' . $oAccount->Email());
+        }
 
-	/**
-	 * Change password
-	 *
-	 * @param \RainLoop\Account $oAccount
-	 * @param string $sPrevPassword
-	 * @param string $sNewPassword
-	 * @return bool
-	 */
-	public function ChangePassword(\RainLoop\Account $oAccount, $sPrevPassword, $sNewPassword)
-	{
-		if($this->oLogger) {
-			$this->oLogger->Write('iMSCP: Try to change password for ' . $oAccount->Email());
-		}
+        $bResult = false;
 
-		$bResult = false;
+        if (!empty($this->sDsn) && 0 < \strlen($this->sUser) && 0 < \strlen($this->sPassword) && $oAccount) {
+            try {
+                $oPdo = new \PDO($this->sDsn, $this->sUser, $this->sPassword);
+                $oPdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                $oStmt = $oPdo->prepare('SELECT mail_pass, mail_addr FROM mail_users WHERE mail_addr = ? LIMIT 1');
 
-		if(!empty($this->sDsn) && 0 < \strlen($this->sUser) && 0 < \strlen($this->sPassword) && $oAccount) {
-			try {
-				$oPdo = new \PDO($this->sDsn, $this->sUser, $this->sPassword);
-				$oPdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                if ($oStmt->execute(array($oAccount->IncLogin())) && $oStmt->rowCount()) {
+                    $aFetchResult = $oStmt->fetch(\PDO::FETCH_ASSOC);
+                    $sDbPassword = \stripslashes($aFetchResult['mail_pass']);
+                    $sPrevPassword = \stripslashes($sPrevPassword);
 
-				$oStmt = $oPdo->prepare('SELECT mail_pass, mail_addr FROM mail_users WHERE mail_addr = ? LIMIT 1');
+                    if ($this->PasswordVerify($sPrevPassword, $sDbPassword)) { # sha512 password (i-MSCP >= 1.4.x)
+                        $sNewPassword = $this->HashPassword($sNewPassword);
+                    } elseif ($sDbPassword !== $sDbPassword) { # Plain password ( iMSCP < 1.4.x)
+                        return $bResult;
+                    }
 
-				if($oStmt->execute(array($oAccount->IncLogin()))) {
-					$aFetchResult = $oStmt->fetchAll(\PDO::FETCH_ASSOC);
+                    $oStmt = $oPdo->prepare('UPDATE mail_users SET mail_pass = ? WHERE mail_addr = ?');
+                    $bResult = (bool)$oStmt->execute(array($sNewPassword, $aFetchResult[0]['mail_addr']));
+                }
+            } catch (\Exception $oException) {
+                if ($this->oLogger) {
+                    $this->oLogger->WriteException($oException);
+                }
+            }
+        }
 
-					if (\is_array($aFetchResult) && isset($aFetchResult[0]['mail_pass'], $aFetchResult[0]['mail_addr'])) {
-						$sDbPassword = \stripslashes($aFetchResult[0]['mail_pass']);
+        return $bResult;
+    }
 
-						if (\stripslashes($sPrevPassword) === $sDbPassword) {
-							$oStmt = $oPdo->prepare('UPDATE mail_users SET mail_pass = ? WHERE mail_addr = ?');
-							$bResult = (bool)$oStmt->execute(array($sNewPassword, $aFetchResult[0]['mail_addr']));
-						}
-					}
-				}
-			} catch(\Exception $oException) {
-				if($this->oLogger) {
-					$this->oLogger->WriteException($oException);
-				}
-			}
-		}
+    /**
+     * Generates a secure random string
+     *
+     * @throws \InvalidArgumentException|\RuntimeException
+     * @param int $length Expected string length
+     * @param string $charList character list to use for string generation (default is Base 64 character set)
+     * @return string
+     */
+    private function RandomStr($length, $charList = self::BASE64)
+    {
+        if (!\extension_loaded('openssl')) {
+            throw new \RuntimeException('OpenSSL extension is not available');
+        }
 
-		return $bResult;
-	}
+        $length = (int)$length;
+        if ($length < 1) {
+            throw new \InvalidArgumentException('Length parameter value must be >= 1');
+        }
+
+        $listLen = \strlen($charList);
+        if ($listLen == 1) {
+            return \str_repeat($charList, $length);
+        }
+
+        $bytes = \openssl_random_pseudo_bytes($length);
+        $pos = 0;
+        $str = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $pos = ($pos + \ord($bytes[$i])) % $listLen;
+            $str .= $charList[$pos];
+        }
+
+        return $str;
+    }
+
+    /**
+     * Create a hash of the given password using the SHA-512 algorithm
+     *
+     * @param string $password The password to be hashed
+     * @return string
+     */
+    private function HashPassword($password)
+    {
+        return \crypt($password, '$6$rounds=' . \sprintf('%1$04d', \rand(3000, 5000)) . '$' . static::RandomStr(16));
+    }
+
+    /**
+     * Timing attack safe string comparison
+     *
+     * @see hash_equals()
+     * @param string $knownString The string of known length to compare against
+     * @param string $userString The user-supplied string
+     * @return bool
+     */
+    private function HashEqual($knownString, $userString)
+    {
+        $knownString = (string)$knownString;
+        $userString = (string)$userString;
+
+        if (\function_exists('hash_equals')) {
+            return \hash_equals($knownString, $userString);
+        }
+
+        $lenExpected = \strlen($knownString);
+        $lenActual = \strlen($userString);
+        $len = \min($lenExpected, $lenActual);
+        $result = 0;
+
+        for ($i = 0; $i < $len; $i++) {
+            $result |= \ord($knownString[$i]) ^ \ord($userString[$i]);
+        }
+
+        $result |= $lenExpected ^ $lenActual;
+        return ($result === 0);
+    }
+
+    /**
+     * Verify the given password against the given hash
+     *
+     * @throws \InvalidArgumentException
+     * @param string $password The password to be checked
+     * @param string $hash The hash to be checked against
+     * @return bool
+     */
+    private function PasswordVerify($password, $hash)
+    {
+        return $this->HashEqual($hash, \crypt($password, $hash));
+    }
 }
--- iMSCP/src/data/_data_11c052c218cd2a2febbfb268624efdc1/_default_/plugins/imscp-change-password/index.php
+++ iMSCP/src/data/_data_11c052c218cd2a2febbfb268624efdc1/_default_/plugins/imscp-change-password/index.php
@@ -22,81 +22,87 @@
  */
 class ImscpChangePasswordPlugin extends \RainLoop\Plugins\AbstractPlugin
 {
-	/**
-	 * Initialization
-	 *
-	 * @return void
-	 */
-	public function Init()
-	{
-		$this->addHook('main.fabrica', 'MainFabrica');
-	}
+    /**
+     * Initialization
+     *
+     * @return void
+     */
+    public function Init()
+    {
+        $this->addHook('main.fabrica', 'MainFabrica');
+    }
 
-	/**
-	 * Check for requirements
-	 *
-	 * @return string
-	 */
-	public function Supported()
-	{
-		if(!extension_loaded('pdo') || !class_exists('PDO')) {
-			return 'The PHP extension PDO (mysql) must be installed to use this plugin';
-		}
+    /**
+     * Check for requirements
+     *
+     * @return string
+     */
+    public function Supported()
+    {
+        if (!extension_loaded('pdo') || !class_exists('PDO')) {
+            return 'The PHP extension PDO (mysql) must be installed to use this plugin';
+        }
 
-		$aDrivers = \PDO::getAvailableDrivers();
-		if(!is_array($aDrivers) || !in_array('mysql', $aDrivers)) {
-			return 'The PHP extension PDO (mysql) must be installed to use this plugin';
-		}
+        $aDrivers = \PDO::getAvailableDrivers();
+        if (!is_array($aDrivers) || !in_array('mysql', $aDrivers)) {
+            return 'The PHP extension PDO (mysql) must be installed to use this plugin';
+        }
 
-		return '';
-	}
+        return '';
+    }
 
-	/**
-	 * Setup
-	 *
-	 * @param string $sName
-	 * @param mixed $oProvider
-	 */
-	public function MainFabrica($sName, &$oProvider)
-	{
-		switch($sName) {
-			case 'change-password':
-				$sDsn = \trim($this->Config()->Get('plugin', 'pdo_dsn', ''));
-				$sUser = (string)$this->Config()->Get('plugin', 'user', '');
-				$sPassword = (string)$this->Config()->Get('plugin', 'password', '');
+    /**
+     * Setup
+     *
+     * @param string $sName
+     * @param mixed $oProvider
+     */
+    public function MainFabrica($sName, &$oProvider)
+    {
+        switch ($sName) {
+            case 'change-password':
+                $sDsn = \trim($this->Config()->Get('plugin', 'pdo_dsn', ''));
+                $sUser = (string)$this->Config()->Get('plugin', 'user', '');
+                $sPassword = (string)$this->Config()->Get('plugin', 'password', '');
 
-				if(!empty($sDsn) && 0 < \strlen($sUser) && 0 < \strlen($sPassword)) {
-					include_once __DIR__ . '/ImscpChangePasswordDriver.php';
+                if (!empty($sDsn) && 0 < \strlen($sUser) && 0 < \strlen($sPassword)) {
+                    include_once __DIR__ . '/ImscpChangePasswordDriver.php';
 
-					$oProvider = new ImscpChangePasswordDriver();
-					$oProvider->SetLogger($this->Manager()->Actions()->Logger());
-					$oProvider->SetConfig($sDsn, $sUser, $sPassword);
-					$oProvider->SetAllowedEmails(\strtolower(\trim($this->Config()->Get('plugin', 'allowed_emails', ''))));
-				}
+                    $oProvider = new ImscpChangePasswordDriver();
+                    $oProvider->SetLogger($this->Manager()->Actions()->Logger());
+                    $oProvider->SetConfig($sDsn, $sUser, $sPassword);
+                    $oProvider->SetAllowedEmails(\strtolower(\trim($this->Config()->Get('plugin', 'allowed_emails', ''))));
+                }
 
-				break;
-		}
-	}
+                break;
+        }
+    }
 
-	/**
-	 * Configuration mapping
-	 *
-	 * @return array
-	 */
-	public function configMapping()
-	{
-		return array(
-			\RainLoop\Plugins\Property::NewInstance('pdo_dsn')->SetLabel('i-MSCP PDO dsn')
-				->SetDefaultValue('mysql:host=127.0.0.1;port=3306;dbname=imscp'),
-			\RainLoop\Plugins\Property::NewInstance('user')->SetLabel('DB User')
-				->SetDefaultValue('rainloop_user'),
-			\RainLoop\Plugins\Property::NewInstance('password')->SetLabel('DB Password')
-				->SetType(\RainLoop\Enumerations\PluginPropertyType::PASSWORD)
-				->SetDefaultValue(''),
-			\RainLoop\Plugins\Property::NewInstance('allowed_emails')->SetLabel('Allowed emails')
-				->SetType(\RainLoop\Enumerations\PluginPropertyType::STRING_TEXT)
-				->SetDescription('Allowed emails, space as delimiter, wildcard supported. Example: user1@domain1.net user2@domain1.net *@domain2.net')
-				->SetDefaultValue('*')
-		);
-	}
+    /**
+     * Configuration mapping
+     *
+     * @return array
+     */
+    public function configMapping()
+    {
+        return array(
+            \RainLoop\Plugins\Property::NewInstance('pdo_dsn')
+                ->SetLabel('i-MSCP PDO dsn')
+                ->SetType(\RainLoop\Enumerations\PluginPropertyType::STRING)
+                ->SetDefaultValue('mysql:host=localhost;port=3306;dbname=imscp'),
+            \RainLoop\Plugins\Property::NewInstance('user')
+                ->SetLabel('i-MSCP DB User')
+                ->SetType(\RainLoop\Enumerations\PluginPropertyType::STRING)
+                ->SetDefaultValue('rainloop_user'),
+            \RainLoop\Plugins\Property::NewInstance('password')
+                ->SetLabel('i-MSCP DB Password')
+                ->SetType(\RainLoop\Enumerations\PluginPropertyType::PASSWORD)
+                ->SetDefaultValue(''),
+            \RainLoop\Plugins\Property::NewInstance('allowed_emails')
+                ->SetLabel('Allowed emails')
+                ->SetType(\RainLoop\Enumerations\PluginPropertyType::STRING_TEXT)
+                ->SetDescription('Allowed emails, space as delimiter, wildcard supported. Example: user1@domain1.net user2@domain1.net *@domain2.net')
+                ->SetDefaultValue('*')
+        );
+    }
 }
